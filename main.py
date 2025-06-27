a42d5fd72df558786a0bddca4e836ea19360e936
Shakil <121shakilahmed@gmail.com>
2025-06-27 12:56:25 +0600

    .

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-service = service(execcutable_path="")
-driver = webdriver.CChrome(service=service)
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
